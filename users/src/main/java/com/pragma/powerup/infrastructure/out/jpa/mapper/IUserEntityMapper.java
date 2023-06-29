155bf4fe8ec8e47be5ce513c114221c92115d4d0
sebastian.giraldo2 <sebastian.giraldo@pragma.com.co>
2023-06-29 13:43:23 -0500

    crear propietario

--- users/src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IUserEntityMapper.java
+++ users/src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IUserEntityMapper.java
@@ -1,19 +1,2 @@
-package com.pragma.powerup.infrastructure.out.jpa.mapper;
-
-import com.pragma.powerup.domain.model.User;
-import com.pragma.powerup.infrastructure.out.jpa.entity.UserEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
-
-import java.util.List;
-import java.util.Optional;
-
-@Mapper(componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        unmappedSourcePolicy = ReportingPolicy.IGNORE
-)
-public interface IUserEntityMapper {
-    UserEntity toEntity(User user);
-    User toUser(UserEntity userEntity);
-    List<User> toUserList(List<UserEntity> userEntityList);
+package com.pragma.powerup.infrastructure.out.jpa.mapper;public interface IUserEntityMapper {
 }
