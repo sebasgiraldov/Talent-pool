e0ba29d9696a3afd9d8d446c7ccb33d931ff08dc
sebastian.giraldo2 <sebastian.giraldo@pragma.com.co>
2023-06-29 09:56:00 -0500

    microservices

---
+++ users/build/generated/sources/annotationProcessor/java/main/com/pragma/powerup/application/mapper/IUserRequestMapperImpl.java
@@ -0,0 +1,34 @@
+package com.pragma.powerup.application.mapper;
+
+import com.pragma.powerup.application.dto.request.UserRequestDto;
+import com.pragma.powerup.domain.model.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-29T09:41:17-0500",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.18 (Oracle Corporation)"
+)
+@Component
+public class IUserRequestMapperImpl implements IUserRequestMapper {
+
+    @Override
+    public User toUser(UserRequestDto userRequestDto) {
+        if ( userRequestDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userRequestDto.getName() );
+        user.setLastname( userRequestDto.getLastname() );
+        user.setDocument( userRequestDto.getDocument() );
+        user.setPhone( userRequestDto.getPhone() );
+        user.setBirthDate( userRequestDto.getBirthDate() );
+        user.setEmail( userRequestDto.getEmail() );
+        user.setPass( userRequestDto.getPass() );
+
+        return user;
+    }
+}
